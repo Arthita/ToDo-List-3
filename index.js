83655cbf605b36ed04522c43c790c6ab4f3c011d
Arthita Paul <arthitapaul@Arthitas-MacBook-Air.local>
2025-07-06 23:41:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const addBtn = document.getElementById("add-btn");
+const inputValue = document.getElementById("input-value");
+const listItems = document.getElementById("list-items");
+
+let tasks = [];
+
+addBtn.addEventListener("click", () => {
+  tasks.push(inputValue.value);
+  localStorage.setItem("value", JSON.stringify(tasks));
+  inputValue.value = "";
+  renderTasks();
+});
+
+listItems.addEventListener("click", (e) => {
+  tasks = tasks.filter((task) => task != e.target.id);
+  localStorage.setItem("value", JSON.stringify(tasks));
+  renderTasks();
+});
+
+function renderTasks() {
+  const storedList = JSON.parse(localStorage.getItem("value"));
+  if (storedList) {
+    let addedTasks = ``;
+    for (let task of storedList) {
+      addedTasks += `<li>${task} <button id=${task} class="remove-btn">x</button></li>`;
+    }
+    listItems.innerHTML = addedTasks;
+  }
+}
+
+renderTasks();
